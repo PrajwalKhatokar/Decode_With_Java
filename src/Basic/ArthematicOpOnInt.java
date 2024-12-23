93da3eb067444dd23cc89b06b0ad23eb7993f9f5
PrajwalKhatokar <prajwal4830@gmail.com>
2024-12-23 14:31:34 +0530

    doing change at 1 :31

--- src/Basic/ArthematicOpOnInt.java
+++ src/Basic/ArthematicOpOnInt.java
@@ -3,10 +3,10 @@ package Basic;
 public class ArthematicOpOnInt {
     public static void main(String[] args) {
         int a=5,b=2;
+        //for now let be the 4 athematic operators we are using
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
-        System.out.println(a%b);
         System.out.println(a/b); //we geting as 2 because int /int=result
         //how to solve this issue .? use double ok.DoubleUsage.java
 
