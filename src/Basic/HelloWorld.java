b317634b113a29fb3e4fbc9c2d7d49a13f65a045
PrajwalKhatokar <prajwal4830@gmail.com>
2025-01-05 11:00:09 +0530

    doing change at 1 :31

--- src/Basic/HelloWorld.java
+++ src/Basic/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
         System.out.println("welcome to the coding world of the java");
         System.out.println("be consistent and be regular");
         System.out.println("do lot and lot of the practise");
-        System.out.println("do code everyday");
+        System.out.println("do code everydayy");
     }
 }
