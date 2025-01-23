d36ffd1340cae3e391c0254880957f339c6fd5f5
PrajwalKhatokar <prajwal4830@gmail.com>
2025-01-23 11:40:09 +0530

    practising reverse of string

--- src/String/reverse.java
+++ src/String/reverse.java
@@ -1,11 +1,22 @@
 package String;
 
-public class reverse {
+public class reverse{
     public static void main(String[] args) {
         String str="hello";
+        // convert this String into the character array ok na so what is do means i use the
+        //toCharArray and its return type is charArray ok na simple
         char[] ch=str.toCharArray();
+        //use for loop for reversing ok na
         for (int i=ch.length-1;i>=0;i--){
-            System.out.println(ch[i]);
+            System.out.print(ch[i]);// ondh ondh 4 ,3,2,1,0 endha iteratate haguthe
+            // 4 nali o
+            // 3 nali l
+            //2 nali l
+            //1 nali e
+            //0 nali h
+             // etara it wil print super ha
+
+            //sout(ch) //etara kotre it wil print only hello 5 times length 5 edhe ala so
         }
     }
 }
