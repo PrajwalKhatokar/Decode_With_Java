03d519ab5aed8bb2d83c5a8bb9d0df36bf5ce922
PrajwalKhatokar <prajwal4830@gmail.com>
2025-01-30 12:22:22 +0530

    here if i give the index i get the character

---
+++ src/Basic/CharData.java
@@ -0,0 +1,12 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class CharData {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch=sc.next().charAt(2);//here if i give the index i get the character
+        System.out.println(ch);
+
+    }
+}
