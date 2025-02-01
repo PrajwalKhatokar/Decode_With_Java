6dd0778f0756f7da55cbbde96106700d86a6fae1
PrajwalKhatokar <prajwal4830@gmail.com>
2025-02-01 10:13:10 +0530

    arthematic progression

---
+++ src/loop/AP.java
@@ -0,0 +1,14 @@
+package loop;
+import java.util.*;
+public class AP {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=0;
+        for(int i=4;i<n;i++){
+            a=a+3;
+            System.out.println(a);
+        }
+
+    }
+}
