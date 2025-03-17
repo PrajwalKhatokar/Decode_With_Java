91cbcf21ae7ae895969220f43aceca48b8a52f09
PrajwalKhatokar <prajwal4830@gmail.com>
2025-03-17 09:58:17 +0530

    reversee an array understandable logic program

---
+++ src/Ashok_it/Reversee.java
@@ -0,0 +1,17 @@
+package Ashok_it;
+
+public class Reversee {
+    public static void main(String[] args) {
+        int [] arr={15,8,9,2,11,4};
+        for (int i=0;i<arr.length/2;i++){
+            int temp=0;
+            temp=arr[i];
+            arr[i]=arr[arr.length-1-i];//munde ogake
+            arr[arr.length-1-i]=temp;
+
+        }
+        for (int n:arr){
+            System.out.println(n +" ");
+        }
+    }
+}
