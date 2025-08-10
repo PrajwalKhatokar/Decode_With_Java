d96e40d44dcfbfca593474e4464da04643162aef
PrajwalKhatokar <prajwal4830@gmail.com>
2025-08-10 09:51:01 +0530

    Linear search

---
+++ src/ArrayPwSkill/LinearSearch.java
@@ -0,0 +1,39 @@
+package ArrayPwSkill;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the target element");
+        int target = sc.nextInt();
+
+        System.out.println("enter the size of the array");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        int n = arr.length;
+        //taking input
+        System.out.println("enter the array elements");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // logic starts
+        boolean flag = false;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == target) {  //if curent ele is equal to target element
+                flag = true; //if got break the loop
+                break;
+            }
+        }
+
+            if (flag == true) {
+                System.out.println("target element found:");
+            } else {
+                System.out.println("element not found");
+            }
+
+        }
+    }
