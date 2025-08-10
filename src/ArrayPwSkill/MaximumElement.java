6065c3c33b1f61e63cfed7193d6a42301f3e0645
PrajwalKhatokar <prajwal4830@gmail.com>
2025-08-10 22:37:49 +0530

    Maximum ELement in an Array

---
+++ src/ArrayPwSkill/MaximumElement.java
@@ -0,0 +1,18 @@
+package ArrayPwSkill;
+
+public class MaximumElement {
+    public static void main(String[] args) {
+
+
+        int[] arr = {10,399,455,22,1000,43,49};
+        int max=arr[0];
+        int size=arr.length;
+        for (int i=0;i<size;i++){
+            if(arr[i]>
+                    max){
+                max=arr[i];
+            }
+        }
+        System.out.println("MaximumElement is:" +max);
+    }
+}
