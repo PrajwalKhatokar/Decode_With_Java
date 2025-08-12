434b31db264c142b32a98b6cf1f80313c2405663
PrajwalKhatokar <prajwal4830@gmail.com>
2025-08-12 10:26:30 +0530

    Maximum and minimum number in an Array

--- src/Ashok_it_Array/Min_Max_2.java
+++ src/Ashok_it_Array/Min_Max_2.java
@@ -1,22 +1,23 @@
 package Ashok_it_Array;
 
-public class Min_Max_2 {
-    public static void main(String[] args) {
-
-
-        int arr[] = {15, 8, 9, 2, 11, 4};
-        int min = arr[0]; //15
-        int max = arr[0];  //15
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            } if (arr[i] < min) {
-                min = arr[i];
-
+        public class Min_Max_2 {
+            public static void main(String[] args) {
+                int arr[]={10,40,50,45,789,900,9,1};
+                int min=arr[0];
+                int max=arr[0];
+                int size=arr.length;
+                for (int i=0;i<size;i++) {
+                    if (arr[i] > max) {
+                        max = arr[i];
+                    } else if (arr[i] < min){
+                        min = arr[i];
+                    }
+                }
+                System.out.println("maximum number in the array is :" +max);
+                System.out.println("minimum number in the array is :" +min);
             }
         }
-            System.out.println(min + " " + max);
 
-
-    }
-}
+//        Output:
+//        maximum number in the array is :900
+//        minimum number in the array is :1
