67d762b39d7c6e052da3530919382947ae1d7a55
PrajwalKhatokar <prajwal4830@gmail.com>
2025-09-06 18:06:25 +0530

    In Sorrted Array find the Number Of Occurence.

---
+++ src/TapArrays/SortedArrayNumberOccurence.java
@@ -0,0 +1,43 @@
+package TapArrays;
+
+import java.util.Scanner;
+
+public class SortedArrayNumberOccurence {
+
+    static void  NumberOfOcuurenceSortedarray(int[] arr){
+        int count=1;
+        for (int i=0;i<arr.length-1;i++) {
+            if (arr[i] == arr[i + 1]) {
+                count++;
+            } else {
+                System.out.println(arr[i] + " " + count);
+                count = 1;
+            }
+        }
+        System.out.println(arr[arr.length-1] + "  " +count);
+
+    }
+    public static void main(String[] args) {
+        System.out.println("enter the size of the array");
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("enter the Array elements");
+        int[] arr = new int[n];
+        for (int i=0;i<arr.length;i++) {
+            arr[i] = sc.nextInt();
+        }
+       NumberOfOcuurenceSortedarray(arr);
+    }
+}
+ //Output:
+
+//enter the size of the array
+//8
+//enter the Array elements
+//2 2 2 3 4 4 4 1
+//        2 3
+//        3 1
+//        4 3
+//        1 1
+
+
