cf30edf696f0beabb250957913e4f2ae6bce451a
PrajwalKhatokar <prajwal4830@gmail.com>
2026-02-12 08:43:54 +0530

    Reverse a  String

---
+++ src/JSixTechnologiesTraineCode/ReverseString.java
@@ -0,0 +1,16 @@
+package JSixTechnologiesTraineCode;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter a String");
+        String s=sc.nextLine();
+        String reverse="";
+        for(int i=s.length()-1;i>=0;i--){
+            reverse=reverse+s.charAt(i);
+        }
+        System.out.println("reverse of the String is:"+reverse);
+    }
+}
