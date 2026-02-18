aeb24103500e44d783f0a6cebefdcaeacd213df2
PrajwalKhatokar <prajwal4830@gmail.com>
2026-02-19 01:14:48 +0530

    Smallest element in the Array

---
+++ src/JSixTechnologiesTraineCode/Arrays/ArrayBasics/SmallestElement.java
@@ -0,0 +1,34 @@
+package JSixTechnologiesTraineCode.Arrays.ArrayBasics;
+
+import java.util.Scanner;
+
+public class SmallestElement {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Size of the Array");
+        int n= sc.nextInt();
+        int  arr[]= new int[n];
+        // for input
+        System.out.println("enter the array elements");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt(); // element we are inputing and storing in arr[i]
+        }
+
+        //logic
+
+        int min=arr[0];
+        for (int i=1;i<n;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
+
+//output
+//Enter the Size of the Array
+//5
+//enter the array elements
+//10 30 90 70 14
+//        10
