091c5698923ad8470deffea9d2bed22e1b93e428
PrajwalKhatokar <prajwal4830@gmail.com>
2026-02-22 19:38:53 +0530

    Prime Number

---
+++ src/JSixTechnologiesTraineCode/BasicLoops/PrimeNumber.java
@@ -0,0 +1,29 @@
+package JSixTechnologiesTraineCode.BasicLoops;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n = sc.nextInt();
+        boolean isprime=true;
+        if(n<=1){
+            isprime=false;
+        }
+        else{
+            for(int i=2;i<=n/2;i++){
+                if(n%i==0){
+                    isprime=false;
+                    break;
+                }
+            }
+        }
+        if(isprime){
+            System.out.println("prime number");
+        }
+        else{
+            System.out.println("not a prime Number");
+        }
+    }
+}
