f73c6539288874905fb72d5c325c271947a418fd
PrajwalKhatokar <prajwal4830@gmail.com>
2026-02-26 00:14:08 +0530

    Majority element

---
+++ src/JSixTechnologiesTraineCode/Arrays/ArrayIntermediatePart1/MajorityElement.java
@@ -0,0 +1,47 @@
+package JSixTechnologiesTraineCode.Arrays.ArrayIntermediatePart1;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of the Array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        // for inputing
+        System.out.println("Enter the array Elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        //logic starts
+        HashMap<Integer,Integer> map = new HashMap<>();//to store key and value pair
+
+        for(int i=0;i<n;i++){
+
+            // get old count or 0 if not present
+            int count = map.getOrDefault(arr[i],0);
+
+            // increase count
+            count++;
+
+            // store updated count
+            map.put(arr[i],count);
+
+            // check if majority
+            if(count > n/2){
+                System.out.println("Majority element: " + arr[i]);
+                return;
+            }
+        }
+
+        System.out.println("No majority element");
+    }
+}
+//Enter the Size of the Array
+//5
+//Enter the array Elements
+//1 1 1 2 3
+//Majority element: 1
