37831c552e8e20c2baef3b982422e2a52133f88a
PrajwalKhatokar <prajwal4830@gmail.com>
2026-03-06 23:47:23 +0530

    Floyds Triangle

---
+++ src/PatternPrintingPwskills/SpecialPatternPartOne/FloydsTriangle.java
@@ -0,0 +1,26 @@
+package PatternPrintingPwskills.SpecialPatternPartOne;
+
+import java.util.Scanner;
+
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        int k=1;
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j<=i; j++) {
+
+                System.out.print(k + " ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
+//enter a number
+//4
+//        1
+//        2 3
+//        4 5 6
+//        7 8 9 10
