0caf3388ad879d71ca7740f312677485330d987c
PrajwalKhatokar <prajwal4830@gmail.com>
2026-03-07 23:26:20 +0530

    AlphabetSquare

---
+++ src/PatternPrintingPwskills/Square/AlphabetSquare.java
@@ -0,0 +1,25 @@
+package PatternPrintingPwskills.Square;
+
+import java.util.Scanner;
+
+public class AlphabetSquare {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int m=sc.nextInt();
+        for (int i=1;i<=m;i++) {
+            for (int j = 1; j <=m; j++) {
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+//Output:
+
+//enter a number
+//4
+//A B C D
+//A B C D
+//A B C D
+//A B C D
