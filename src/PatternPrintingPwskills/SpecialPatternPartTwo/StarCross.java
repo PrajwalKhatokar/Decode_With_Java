ce27e243116729df943e2cf6345ec389b4123ec3
PrajwalKhatokar <prajwal4830@gmail.com>
2026-03-09 23:19:05 +0530

    Star Cross

---
+++ src/PatternPrintingPwskills/SpecialPatternPartTwo/StarCross.java
@@ -0,0 +1,26 @@
+package PatternPrintingPwskills.SpecialPatternPartTwo;
+
+import java.util.Scanner;
+
+public class StarCross {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        int mid = n/2 + 1;
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j || i+j==n+1) {
+                    System.out.print("*" + " ");
+                } else {
+                    System.out.print(" " + " ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// cross star output
