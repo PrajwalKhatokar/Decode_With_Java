450d9f2345935f4611df7af8a78e34126c45dcd8
PrajwalKhatokar <prajwal4830@gmail.com>
2026-03-11 00:26:03 +0530

    Two Sum

--- src/JSixTechnologiesTraineCode/Arrays/ArrayIntermediatePart2/TwoSum.java
+++ src/JSixTechnologiesTraineCode/Arrays/ArrayIntermediatePart2/TwoSum.java
@@ -8,6 +8,8 @@ package JSixTechnologiesTraineCode.Arrays.ArrayIntermediatePart2;
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.</br>
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.</br>
 //
+//means i should get the only one pair which is equal to the target and return there index</br>
+//by storing in the array for this i can also do optimization for now not required</br>
 //You can return the answer in any order.</br>
 //Example 1:</br>
 //</br>
