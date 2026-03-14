2f0a2fceac5573dbafe9edd06100a938eb87b4a2
PrajwalKhatokar <prajwal4830@gmail.com>
2026-03-15 00:00:03 +0530

    Sort Zeros and One's

--- src/JSixTechnologiesTraineCode/Arrays/ArrayIntermediatePart2/SortZeroOne.java
+++ src/JSixTechnologiesTraineCode/Arrays/ArrayIntermediatePart2/SortZeroOne.java
@@ -2,6 +2,21 @@ package JSixTechnologiesTraineCode.Arrays.ArrayIntermediatePart2;
 
 public class SortZeroOne {
     public static void main(String[] args) {
+
+//        logic
+        // logic
+//        0 1 2 3 4 5 6
+//        0 0 1 0 1 0 1
+//        arr[i]=0; starting element hey 0 edhaare
+//        arr[j]=1; last element q edhare
+//        1st reverse logic so taat i can sort it wont cross i<j
+//        i>j adre break, becuase mathe unsorted hagbiduthe nodu down if en madtuhe mathe sort madi unsorted koduthe
+//        when i swap
+//        if(arr[i]==1 && arr[j]==0){
+//            arr[i]=0;
+//            arr[j]=1;
+//
+//        }
         int[] arr={0,1,0,1,0,0,0,1,0,1,0,1};
         int n=arr.length;
         //solution logic starts
