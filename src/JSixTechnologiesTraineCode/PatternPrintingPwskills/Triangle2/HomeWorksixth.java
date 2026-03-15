02df08e5d78db7477578865f7090a63abcdce65c
PrajwalKhatokar <prajwal4830@gmail.com>
2026-03-15 11:25:52 +0530

    Pattern printing

---
+++ src/JSixTechnologiesTraineCode/PatternPrintingPwskills/Triangle2/HomeWorksixth.java
@@ -0,0 +1,31 @@
+package JSixTechnologiesTraineCode.PatternPrintingPwskills.Triangle2;
+
+import java.util.Scanner;
+
+public class HomeWorksixth{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int m=sc.nextInt();
+        for (int i=1;i<=m;i++) {
+            for (int j = 1; j <= i; j++) {
+                if(i%2!=0) {
+                    System.out.print(j + " ");
+                }
+                else{
+                    System.out.print((char)(j+64)+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+//enter a number
+//
+//5
+// 1
+//A B
+//1 2 3
+//A B C D
+//1 2 3 4 5
