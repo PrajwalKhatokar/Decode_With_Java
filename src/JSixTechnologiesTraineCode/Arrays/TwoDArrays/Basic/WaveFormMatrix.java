ce3322c26fe17bcdf693e086c7c06bce3f7eaecc
PrajwalKhatokar <prajwal4830@gmail.com>
2026-04-09 09:47:31 +0530

    waveform matrix

---
+++ src/JSixTechnologiesTraineCode/Arrays/TwoDArrays/Basic/WaveFormMatrix.java
@@ -0,0 +1,47 @@
+package JSixTechnologiesTraineCode.Arrays.TwoDArrays.Basic;
+
+public class WaveFormMatrix {
+
+    public static void print(int[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        //transpose of matrix without using the extra array ok na
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
+        print(arr);
+        int m=arr.length;
+        int n=arr[0].length;
+        for(int i=0;i<m;i++){
+          if(i%2==0){
+           for(int j=0;j<n;j++) {
+               System.out.print(arr[i][j] + " ");
+           }
+
+       }
+           else{
+           for(int j=n-1;j>=0;j--) {
+               System.out.print(arr[i][j] + " ");
+           }
+
+           }
+            System.out.println();
+       }
+    }
+}
+
+//        1 2 3
+//        4 5 6
+//        7 8 9
+//
+//        1 2 3
+//        6 5 4
+//        7 8 9
