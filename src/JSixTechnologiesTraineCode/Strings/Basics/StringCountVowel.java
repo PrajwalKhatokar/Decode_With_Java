b60cdc8e5e5aba0def27075fa91fb8f29ae48c5f
PrajwalKhatokar <prajwal4830@gmail.com>
2026-04-17 10:23:22 +0530

    Count Vowels

---
+++ src/JSixTechnologiesTraineCode/Strings/Basics/StringCountVowel.java
@@ -0,0 +1,33 @@
+package JSixTechnologiesTraineCode.Strings.Basics;
+
+public class StringCountVowel {
+
+    public static boolean isvowels(char ch) {
+        if (ch == 'a' || ch == 'A') return true;
+        if (ch == 'e' || ch == 'E') return true;
+        if (ch == 'i' || ch == 'I') return true;
+        if (ch == 'o' || ch == 'O') return true;
+        if (ch == 'u' || ch == 'U') return true;
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "aeiou";
+        int n = s.length();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i); // i nned character i passing the index
+            if (isvowels(ch) == true) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
+}
+
+//output : 5
