46ba0bc3a9e47eeb8dbf804bf34c75061f669f3e
PrajwalKhatokar <prajwal4830@gmail.com>
2026-04-22 10:02:33 +0530

    Isomorphic

--- src/JSixTechnologiesTraineCode/Strings/Lifesaver/Isomorphic.java
+++ src/JSixTechnologiesTraineCode/Strings/Lifesaver/Isomorphic.java
@@ -9,7 +9,7 @@ public class Isomorphic {
 
         if (s.length() != t.length()) {
             System.out.println(false);
-            return;
+            return;  //jvm na stopping length hey equal hagila andre inside ogodh waste anta.
         }
 
         HashMap<Character, Character> map1 = new HashMap<>();
@@ -24,7 +24,8 @@ public class Isomorphic {
 
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-
+//first haku memory nali key and value na ie else part nodkoluthe if nali mismatch adre
+            // c1 and c2 ie paper hakide tiler nodu p nali t ne erbeku but l edhe mismatch ok na false return and break the loop.
             if (map1.containsKey(c1)) {
                 if (map1.get(c1) != c2) {
                     System.out.println(false);
@@ -33,7 +34,10 @@ public class Isomorphic {
             } else {
                 map1.put(c1, c2);
             }
-
+            // eli reverse ge reverse edhare no worries bere edhare false
+            //also one to one mapping we checking ie reverse mapping
+//          Yes — we are checking whether the target character is already mapped
+//           and if it is mapped to a DIFFERENT source → then it is invalid
             if (map2.containsKey(c2)) {
                 if (map2.get(c2) != c1) {
                     System.out.println(false);
