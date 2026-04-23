e9ebf7c78a12e3bf31ba53f39d95853355ee9ab4
PrajwalKhatokar <prajwal4830@gmail.com>
2026-04-23 13:02:33 +0530

    Count Word Frequency count

--- src/JSixTechnologiesTraineCode/Strings/Lifesaver/CountWordFrequencyinSentence.java
+++ src/JSixTechnologiesTraineCode/Strings/Lifesaver/CountWordFrequencyinSentence.java
@@ -7,5 +7,23 @@ public class CountWordFrequencyinSentence {
     public static void main(String args[]){
 
 
+            // i need to find the frequency in the String Word not the character
+
+            Scanner sc= new Scanner(System.in);
+            String s= sc.nextLine();
+
+            String[] words=s.split(" "); //// vvip split into WORDS (not characters) means " " etara erbeku "" etara edhare character frequency koduthe
+
+            HashMap<String,Integer> map = new HashMap<>();
+
+            for(int i=0;i<words.length;i++){ // word length beku so words.length not s.length
+                String word=words[i];
+                map.put(word,map.getOrDefault(word,0)+1);
+            }
+
+            for(String key:map.keySet()){
+                System.out.println(key+ " -> " +map.get(key) );
+            }
+
     }
 }
