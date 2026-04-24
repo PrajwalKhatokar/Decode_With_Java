5ea69cd12a6ec00263aa60bd8e8c1789c7003903
PrajwalKhatokar <prajwal4830@gmail.com>
2026-04-24 14:14:52 +0530

    Basic Sorting

---
+++ src/JSixTechnologiesTraineCode/Sorting/BasicSorting.java
@@ -0,0 +1,20 @@
+package JSixTechnologiesTraineCode.Sorting;
+
+public class BasicSorting{
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,56,0};
+        int n=arr.length;
+        boolean flag = true;
+        for(int i = 0; i<n-1;i++){
+            if(arr[i] > arr[i+1]){
+                flag=false;
+                break;
+            }
+        }
+        if(flag==true) System.out.println("sorted");
+        else System.out.println("not sorted");
+    }
+}
+
+
+//not sorted
