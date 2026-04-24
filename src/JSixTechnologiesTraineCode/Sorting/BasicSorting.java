1581cb1277d3ccdd793d68b675299568f49f3ff2
PrajwalKhatokar <prajwal4830@gmail.com>
2026-04-24 14:17:02 +0530

    Basic Sorting

--- src/JSixTechnologiesTraineCode/Sorting/BasicSorting.java
+++ src/JSixTechnologiesTraineCode/Sorting/BasicSorting.java
@@ -1,6 +1,7 @@
 package JSixTechnologiesTraineCode.Sorting;
 
 public class BasicSorting{
+    // for notes for fast recap refer A4 size College Book ds shot book or drive4830 jsix,sorting
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,56,0};
         int n=arr.length;
