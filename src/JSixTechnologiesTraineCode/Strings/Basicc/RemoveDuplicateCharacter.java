77b1ef47b7381dc8d691eac93fc89cd4e5ec9b68
PrajwalKhatokar <prajwal4830@gmail.com>
2026-04-28 16:44:07 +0530

    Remove duplicate character

---
+++ src/JSixTechnologiesTraineCode/Strings/Basicc/RemoveDuplicateCharacter.java
@@ -0,0 +1,32 @@
+package JSixTechnologiesTraineCode.Strings.Basicc;
+import java.util.*;
+
+public class RemoveDuplicateCharacter {
+    public static void main(String[] args) {
+
+                Scanner sc = new Scanner(System.in);
+                String s = sc.nextLine();
+
+                // store visited characters
+                HashSet<Character> seen = new HashSet<>();  //Create memory to store unique characters
+                                                             // “I will remember what I have already seen”
+                                                             // seen = {}
+                // build result
+                StringBuilder result = new StringBuilder();  //Create empty box to build answer
+
+                for (int i = 0; i < s.length(); i++) {
+
+                    char curr = s.charAt(i);
+
+                    // if not seen → add
+                    if (!seen.contains(curr)) {  //“Have I NOT seen this before?” then add
+                        seen.add(curr);  // nodidre add to seen
+                        result.append(curr); // nodidre add to result
+                    }
+                }
+
+                System.out.println(result.toString());
+            }
+        }
+//Banana
+// Ban
